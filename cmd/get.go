c6919376fd93b4443f17e7ccd6de602391b88d6d
halfbreak <14543067+halfbreak@users.noreply.github.com>
2020-06-12 17:58:46 +0100

    Introduced Exchange interface. BitStamp is now a struct.

--- cmd/get.go
+++ cmd/get.go
@@ -11,40 +11,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// helloCmd represents the hello command
-var helloCmd = &cobra.Command{
+// getCmd represents the get command
+var getCmd = &cobra.Command{
 	Use:   "get",
 	Short: "Gets info on a currency pair",
 	Long:  `Gets all the current information from the given currency pair, the following one are allowed: btcusd, btceur, btcgbp, btcpax, gbpusd, gbpeur, eurusd, xrpusd, xrpeur, xrpbtc, xrpgbp, xrppax, ltcusd, ltceur, ltcbtc, ltcgbp, ethusd, etheur, ethbtc, ethgbp, ethpax, bchusd, bcheur, bchbtc, bchgbp, paxusd, paxeur, paxgbp`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		currencyPairSlice := []interface{}{"btcusd", "btceur", "btcgbp", "btcpax", "gbpusd", "gbpeur", "eurusd", "xrpusd", "xrpeur", "xrpbtc", "xrpgbp", "xrppax", "ltcusd", "ltceur", "ltcbtc", "ltcgbp", "ethusd", "etheur", "ethbtc", "ethgbp", "ethpax", "bchusd", "bcheur", "bchbtc", "bchgbp", "paxusd", "paxeur", "paxgbp"}
-
-		bitstamp := &model.Exchange{BaseURL: "https://www.bitstamp.net/api/v2/ticker/", CurrencyPairs: currencyPairSlice}
-
-		if bitstamp.IsInvalidCurrencyPair(args[0]) {
-			fmt.Println("Currency pair " + args[0] + " not recognized")
-			os.Exit(1)
-		}
-
-		response, err := http.Get(bitstamp.GetCurrencyPairURL(args[0]))
-
-		if err != nil {
-			fmt.Print(err.Error())
-			os.Exit(1)
-		}
-
-		responseData, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(responseData))
+		exchange := &model.BitStamp{}
+		getCurrency(args[0], exchange)
 	},
 	Args: cobra.MinimumNArgs(1),
 }
 
 func init() {
-	RootCmd.AddCommand(helloCmd)
+	RootCmd.AddCommand(getCmd)
 
 	// Here you will define your flags and configuration settings.
 
@@ -55,4 +36,25 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// helloCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func getCurrency(currencyPair string, exchange model.Exchange) {
+
+	if exchange.IsInvalidCurrencyPair(currencyPair) {
+		fmt.Println("Currency pair " + currencyPair + " not recognized")
+		os.Exit(1)
+	}
+
+	response, err := http.Get(exchange.GetCurrencyPairURL(currencyPair))
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
 }
--- model/exchange.go
+++ model/exchange.go
@@ -5,18 +5,23 @@ import (
 )
 
 // Exchange to query for trades
-type Exchange struct {
-	BaseURL       string
-	CurrencyPairs []interface{}
+type Exchange interface {
+	GetCurrencyPairURL(currencyPair string) string
+	IsInvalidCurrencyPair(currencyPair string) bool
+}
+
+// BitStamp Exchange to query for trades
+type BitStamp struct {
 }
 
 // GetCurrencyPairURL - Get URL to query for the information of a currency pair
-func (e *Exchange) GetCurrencyPairURL(currencyPair string) string {
-	return e.BaseURL + currencyPair
+func (e *BitStamp) GetCurrencyPairURL(currencyPair string) string {
+	return "https://www.bitstamp.net/api/v2/ticker/" + currencyPair
 }
 
 // IsInvalidCurrencyPair - Checks if given currency pair is valid
-func (e *Exchange) IsInvalidCurrencyPair(currencyPair string) bool {
-	currencyPairSet := mapset.NewSetFromSlice(e.CurrencyPairs)
+func (e *BitStamp) IsInvalidCurrencyPair(currencyPair string) bool {
+	currencyPairSlice := []interface{}{"btcusd", "btceur", "btcgbp", "btcpax", "gbpusd", "gbpeur", "eurusd", "xrpusd", "xrpeur", "xrpbtc", "xrpgbp", "xrppax", "ltcusd", "ltceur", "ltcbtc", "ltcgbp", "ethusd", "etheur", "ethbtc", "ethgbp", "ethpax", "bchusd", "bcheur", "bchbtc", "bchgbp", "paxusd", "paxeur", "paxgbp"}
+	currencyPairSet := mapset.NewSetFromSlice(currencyPairSlice)
 	return !currencyPairSet.Contains(currencyPair)
 }
